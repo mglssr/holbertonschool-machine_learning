556c437d981690dbfedf4f8983da81454b379085
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-11-28 19:29:52 +0000

    .

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -4,5 +4,6 @@
 
 def matrix_transpose(matrix):
     """function that returns the transpose of a 2D matrix"""
-    t_matrix = [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
-    return t_matrix
+    t_matrix = [[matrix[i][j] for i in range(len(matrix))]
+                for j in range(len(matrix[0]))]
+    return (t_matrix)
