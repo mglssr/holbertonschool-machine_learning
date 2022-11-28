d68455b8a2bff4193e45af8a4e4e418dce8bacbf
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-11-28 18:07:01 +0000

    docs

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """function that calculates the shape of a matrix"""
     shape = []
     while type(matrix) != int:
         shape.append(len(matrix))
