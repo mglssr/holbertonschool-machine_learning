09c575d42249eb5dc29fe698b474123ca39452dc
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-11-28 20:25:37 +0000

    task 5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,10 +4,8 @@
 
 def add_matrices2D(mat1, mat2):
     """function that adds two matrices element-wise"""
-    if (len(mat1) != len(mat2)):
-        return
-    if (len(mat1[0]) != len(mat2[0])):
+    if (len(mat1) != len(mat2)) or (len(mat1[0]) != len(mat2[0])):
         return
     mat3 = [[(mat1[i][j] + mat2[i][j]) for j in range(len(mat2))]
-            for i in range(len(mat2))]
+            for i in range(len(mat2[0]))]
     return (mat3)
