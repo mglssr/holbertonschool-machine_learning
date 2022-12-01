6536599b03a8fe549777f3459f2a3cbd175b2580
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-01 01:02:37 +0000

    task 16

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 16"""
+import numpy as np
+
+
+def add_matrices(mat1, mat2):
+    """function that adds two matrices"""
+    arr1 = np.array(mat1)
+    arr2 = np.array(mat2)
+    if arr1.shape != arr2.shape:
+        return
+    else:
+        return (np.array(mat1) + np.array(mat2)).tolist()
