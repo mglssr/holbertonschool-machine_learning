ebb447df7e14aef6d57f7ca80efae697953a5118
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-01 14:17:52 +0000

    task 16

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 """Task 16"""
-import numpy as np
+
+
+def matrix_shape(matrix):
+    """function that calculates the shape of a matrix"""
+    shape = []
+    while (type(matrix) != int):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return (shape)
 
 
 def add_matrices(mat1, mat2):
     """function that adds two matrices"""
-    arr1 = np.array(mat1)
-    arr2 = np.array(mat2)
-    if arr1.shape != arr2.shape:
+    arr1 = matrix_shape(mat1)
+    arr2 = matrix_shape(mat2)
+    if arr1 != arr2:
         return
     else:
-        mat3 = (np.array(mat1) + np.array(mat2)).tolist()
+        mat3 = []
+        if len(arr1) == 1:
+            for i in range(arr1[0]):
+                mat3.append(mat1[i] + mat2[i])
+            return (mat3)
+        else:
+            for i in range(arr1[0]):
+                mat3.append(add_matrices(mat1[i], mat2[i]))
         return (mat3)
