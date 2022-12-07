9fa3b23177235731de5fa165674cda3b46f6cc54
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-07 15:56:28 +0000

    task 17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -5,17 +5,18 @@
 def poly_integral(poly, C=0):
     """function that calculates the integral of a polynomial"""
     int_poly = []
-    if type(C) not in [int, float]:
+    if type(C) != int or poly == []:
         return
     int_poly.append(C)
     i = 0
-    while i < len(poly):
-        if type(poly[i]) not in [int, float]:
-            return
-        p = poly[i] / (i + 1)
-        if int(p) == p:
-            int_poly.append(int(p))
-        else:
-            int_poly.append(p)
-        i += 1
+    try:
+        while i < len(poly):
+            p = poly[i] / (i + 1)
+            if int(p) == p:
+                int_poly.append(int(p))
+            else:
+                int_poly.append(p)
+            i += 1
+    except Exception:
+        return
     return (int_poly)
