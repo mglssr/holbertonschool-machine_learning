4b7000d1028ffe75e49609f7a7d7f104cf5c1e2a
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-07 15:43:21 +0000

    task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,7 +6,7 @@ def summation_i_squared(n):
     """ function def summation_i_squared(n):
     that tcalculates sum_{i=1}^{n} i^2 =
     ∑i2=(n)(n+1)(2n+1)6"""
-    if type(n) != int:
+    if type(n) != int or n < 1:
         return
     add = n * (n+1) * (2 * n + 1) / 6
     return (int(add))
