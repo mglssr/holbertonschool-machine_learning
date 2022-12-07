8276f829335f1ad69ee0cdfcfa1baac1296537e2
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-07 15:37:53 +0000

    task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,6 +5,8 @@
 def summation_i_squared(n):
     """ function def summation_i_squared(n):
     that tcalculates sum_{i=1}^{n} i^2:"""
+    if type(n) not in [int, float]:
+        return
     suma = [*range(n + 1)]
     add = sum(map(lambda i: i * i, suma))
     return (add)
