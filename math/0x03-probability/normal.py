1c2d490da1ba4655d4308333f2c03e67e709eee0
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-13 20:47:50 +0000

    task 6

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Normal distribution class"""
+
+
+class Normal():
+    """Normal class distribution definition"""
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """class constructor"""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            else:
+                self.stddev = float(stddev)
+            self.mean = float(mean)
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float(sum(data) / len(data))
+            i = 0
+            s = 0
+            while i < len(data):
+                s += ((data[i] - self.mean) ** 2)
+                i += 1
+            self.stddev = (s / len(data)) ** 0.5
