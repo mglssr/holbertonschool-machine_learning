f5e3c33c1e74b2c96b1318f159e1ab586befdca8
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-13 15:46:09 +0000

    task 1

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Poisson distribution class"""
+import numpy as np
+
+
+class Poisson():
+    """Poisson distribution definition"""
+    def __init__(self, data=None, lambtha=1.):
+        """class contructior"""
+        if not data:
+            if lambtha > 0:
+                self.lambtha = float(lambtha)
+            else:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = np.mean(data)
