d7645ebeee16e4495e76954342e754dccc14ebdc
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-14 20:40:17 +0000

    task 10

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Binomial distribution class"""
+
+
+class Binomial():
+    """Binomial class distribution definition"""
+    def __init__(self, data=None, n=1, p=0.5):
+        """class constructor"""
+        if data is None:
+            if n < 0:
+                raise ValueError("n must be a positive value")
+            else:
+                self.n = int(n)
+            if p > 1 or p < 0:
+                raise ValueError("p must be greater than 0 and less than 1")
+            else:
+                self.p = float(p)
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                mean = float(sum(data) / len(data))
+                s = 0
+                i = 0
+                while i < len(data):
+                    s += (data[i] - mean) ** 2
+                    i += 1
+                var = s / len(data)
+                self.p = ((var / mean) - 1) / -1
+                self.n = round(mean / self.p)
