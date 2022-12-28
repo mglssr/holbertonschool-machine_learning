204403d1e8809e9eb1765e8c499ac927869bb16d
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-28 02:28:44 +0000

    task 8

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""classification"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """defines a neural network with one
+    hidden layer performing binary classification"""
+    def __init__(self, nx, nodes):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = 0
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
