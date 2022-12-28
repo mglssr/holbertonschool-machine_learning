77765a93fdd7d611ac31c6db03e31e0e00fa11fa
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-28 02:34:34 +0000

    rm comments :]

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -16,9 +16,9 @@ class NeuralNetwork():
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        self.W1 = np.random.normal(size=(nodes, nx))  # weight hidden layer
-        self.b1 = np.zeros((nodes, 1))  # bias for the hidden laye
-        self.A1 = 0  # activated output for the hidden layer
-        self.W2 = np.random.normal(size=(1, nodes))  # weights output neuron
-        self.b2 = 0  # bias for the output neuron
-        self.A2 = 0  # activated output for the output neuron
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
