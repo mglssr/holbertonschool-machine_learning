17097d3d244524125d6c139c66f67c602aaa817a
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-30 17:04:20 +0000

    task 16

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-"""classification"""
+"""
+    module
+"""
 import numpy as np
 
 
-class DeepNeuralNetwork():
-    """defines a deep neural network performing binary classification"""
+class DeepNeuralNetwork:
+    """
+    Deep Neural Network
+    """
+
     def __init__(self, nx, layers):
-        """class constructor"""
+        """ init function """
         if type(nx) != int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -16,12 +21,13 @@ class DeepNeuralNetwork():
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        for ly in range(self.L):
-            if type(layers[ly]) != int or layers[ly] < 0:
+        for lay in range(self.L):
+            if type(layers[lay]) != int or layers[lay] < 0:
                 raise TypeError("layers must be a list of positive integers")
-            self.weights[f"b{ly + 1}"] = np.zeros((layers[ly], 1))
-            aux = nx
-            if ly > 0:
-                aux = layers[ly - 1]
-            self.weights[f"W{ly + 1}"] = (np.random.randn(layers[ly], aux)
-                                          * np.sqrt(2/aux))
+            s_l = str(lay + 1)
+            self.weights["b" + s_l] = np.zeros((layers[lay], 1))
+            prev = nx
+            if (lay > 0):
+                prev = layers[lay - 1]
+            self.weights["W" + s_l] = np.random.randn(layers[lay], prev)
+            self.weights["W" + s_l] *= np.sqrt(2/prev)
