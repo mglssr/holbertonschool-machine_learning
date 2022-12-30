4789bfda1b3d787546559a6e564c65e2fa997841
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-30 17:15:38 +0000

    task 17

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """defines a deep neural network with one
+    hidden layer performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for ly in range(self.L):
+            if type(layers[ly]) != int or layers[ly] < 0:
+                raise TypeError("layers must be a list of positive integers")
+            lblb = f"b{ly + 1}"
+            lblw = f"W{ly + 1}"
+            self.__weights[lblb] = np.zeros((layers[ly], 1))
+            aux = nx
+            if (ly > 0):
+                aux = layers[ly - 1]
+            self.__weights[lblw] = (np.random.randn(layers[ly], aux)
+                                       * np.sqrt(2/aux))
+
+    @property
+    def L(self):
+        """comment"""
+        return (self.__L)
+
+    @property
+    def cache(self):
+        """comment"""
+        return (self.__cache)
+
+    @property
+    def weights(self):
+        """comment"""
+        return (self.__weights)
