162697f46d5a09abd0d624936308169ba4fc0fa1
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-01-07 01:42:14 +0000

    task 6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -7,6 +7,7 @@ create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
+
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
           alpha, iterations, save_path="/tmp/model.ckpt"):
     """function  that builds, trains, and saves a neural network classifier"""
@@ -25,7 +26,7 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
 
     saver = tf.train.Saver()
     init = tf.global_variables_initializer()
-    
+
     with tf.Session() as sess:
         sess.run(init)
 
