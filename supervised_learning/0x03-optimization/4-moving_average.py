58f601582dae2644561014169001a7f1fab634b8
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-01-12 12:46:48 +0000

    task 4

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -7,7 +7,7 @@ def moving_average(data, beta):
     """function that calculates the weighted moving average of a data set"""
     wmal = []
     w = 0
-    for i in range(len(data)):
-        w = (beta * data[i] + (1 - beta) * data[i])
-        wmal.append(w / 1 - (beta ** (data[i])))
+    for idx, val in enumerate(data):
+        w = beta * w + (1 - beta) * val
+        wmal.append(w / (1 - beta ** (idx + 1)))
     return wmal
