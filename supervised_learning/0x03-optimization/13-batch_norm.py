86931759ec472198547f0d26af30e945af7ead19
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-01-13 12:32:09 +0000

    task 13

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -6,8 +6,7 @@ import numpy as np
 def batch_norm(Z, gamma, beta, epsilon):
     """function that normalizes an unactivated output
     of a neural network using batch normalization"""
-    mu = np.mean(Z, axis=0)
-    dev = np.var(Z, axis=0)
-    norma = (Z - mu) / (np.sqrt(dev - epsilon))
-    y = (gamma * norma) + beta
-    return (y)
+    mean = np.mean(Z, axis=0)
+    mu = np.var(Z, axis=0)
+    norma = (Z - mean) / np.sqrt(mu + epsilon)
+    return (gamma * norma) + beta
