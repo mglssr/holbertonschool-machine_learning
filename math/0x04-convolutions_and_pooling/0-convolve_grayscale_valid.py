d8ece853392e7ea6dbb18c0ed75ad42f58abd263
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-01-19 12:20:07 +0000

    tasks 0-1

---
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""task 0"""
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """function that performs a valid convolution on grayscale images"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    convh = int(h - kh + 1)
+    convw = int(w - kw + 1)
+    output = np.zeros(shape=(m, convh, convw))
+    for h in range(convh):
+        for w in range(convw):
+            output[:, h, w] = np.sum((images[:, h: h + kh, w: w + kw] * kernel), axis=(1, 2))
+    return (output)
---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""task 1"""
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """function that performs a same convolution on grayscale images"""
+    kh, kw = kernel.shape
+    padh = int((kh - 1) / 2)
+    padw = int((kw - 1) / 2)
+    pading = np.pad(images, ((0, 0), (padh, padh), (padw, padw)), 'constant', constant_values=0)
+    output = np.zeros(shape=images.shape)
+    for h in range(images.shape[1]):
+        for w in range(images.shape[2]):
+            output[:, h, w] = np.sum((pading[:, h: h + kh, w: w + kw] * kernel), axis=(1, 2))
+    return (output)
