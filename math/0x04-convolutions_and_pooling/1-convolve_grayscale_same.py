418af3adb85a1cdd9198789d77847e941dd0734e
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-01-19 12:38:03 +0000

    task 1

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -5,14 +5,13 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """function that performs a same convolution on grayscale images"""
-    kh, kw = kernel.shape
-    padh = np.ceil(((kh, kw) - 1) / 2).astype(int)
-    padw = np.ceil(((kh, kw) - 1) / 2).astype(int)
-    pading = np.pad(images, ((0, 0), (padh, padh), (padw, padw)),
-                    'constant', constant_values=0)
-    output = np.zeros(shape=images.shape)
-    for h in range(output.shape[1]):
-        for w in range(output.shape[2]):
-            output[:, h, w] = np.sum((pading[:, h: h + kh,
-                                             w: w + kw] * kernel), axis=(1, 2))
-    return (output)
+    ks = np.array(kernel.shape)
+    pad = np.ceil((ks - 1) / 2).astype(int)
+    img = np.pad(images, ((0, 0), (pad[0], pad[0]), (pad[1], pad[1])),
+                 'constant', constant_values=0)
+    out = np.zeros(shape=images.shape)
+    for x in range(out.shape[1]):
+        for y in range(out.shape[2]):
+            sp = img[:, x: x + ks[0], y: y + ks[1]]
+            out[:, x, y] = np.sum(sp * kernel, axis=(1, 2))
+    return out
