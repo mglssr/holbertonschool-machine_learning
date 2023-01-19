0d559a8908b9be93d9423cf82ea4107113a320ce
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-01-19 12:31:37 +0000

    task 1

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -6,13 +6,13 @@ import numpy as np
 def convolve_grayscale_same(images, kernel):
     """function that performs a same convolution on grayscale images"""
     kh, kw = kernel.shape
-    padh = int(np.ceil((kh - 1) / 2))
-    padw = int(np.ceil((kh - 1) / 2))
+    padh = int((kh - 1) / 2)
+    padw = int((kw - 1) / 2)
     pading = np.pad(images, ((0, 0), (padh, padh), (padw, padw)),
                     'constant', constant_values=0)
     output = np.zeros(shape=images.shape)
-    for h in range(images.shape[1]):
-        for w in range(images.shape[2]):
+    for h in range(output.shape[1]):
+        for w in range(output.shape[2]):
             output[:, h, w] = np.sum((pading[:, h: h + kh,
                                              w: w + kw] * kernel), axis=(1, 2))
     return (output)
