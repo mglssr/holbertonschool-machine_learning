668129f7537a97909d33994b86662a97c3c88b1d
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-01-25 02:21:41 +0000

    task 2

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""task 2"""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """function that sets up Adam optimization for a keras model
+    with categorical crossentropy loss and accuracy metrics"""
+    optimizer = K.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2
+    )
+    network.compile(loss='categorical_crossentropy',
+                    optimizer=optimizer,
+                    metrics=[K.metrics.Accuracy()])
