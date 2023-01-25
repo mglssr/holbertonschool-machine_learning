969bba18796367abb234d49c9e47b05b56c5cc5c
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-01-25 15:58:40 +0000

    task 6

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -7,8 +7,9 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
                 verbose=True, shuffle=False):
     """function that trains a model using mini-batch gradient descent"""
+    callback = []
     if validation_data is not None and early_stopping:
-        callback = K.callbacks.EarlyStopping(patience=patience)
+        callback.append(K.callbacks.EarlyStopping(patience=patience))
     history = network.fit(data,
                           labels,
                           batch_size=batch_size,
@@ -16,5 +17,5 @@ def train_model(network, data, labels, batch_size, epochs,
                           verbose=verbose,
                           validation_data=validation_data,
                           shuffle=shuffle,
-                          callbacks=[callback])
+                          callbacks=callback)
     return (history)
