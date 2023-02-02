1fd92f3012ac5ca8d1ee2000de2c059ea7e79ca6
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-02-02 14:02:40 +0000

    task 2

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -6,28 +6,33 @@ import numpy as np
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """function that performs back propagation over a convolutional
     layer of a neural network"""
-    m, h_prev, w_prev = A_prev.shape[:3]
-    kh, kw, c_prev, c_new = W.shape
+    m, h, w, _ = A_prev.shape
+    _, zh, zw, zc = dZ.shape
+    kh, kw, _, _ = W.shape
     sh, sw = stride
-    m, h_new, w_new, c_new = dZ.shape
-    if padding == 'same':
-        ph = int((h_prev - 1) * sh + kh - h_prev // 2)
-        pw = int((w_prev - 1) * sw + kw - w_prev // 2)
-    if padding == 'valid':
+    if padding == "same":
+        ph = int(np.ceil(((h - 1) * sh + kh - h) / 2))
+        pw = int(np.ceil(((w - 1) * sw + kw - w) / 2))
+    else:
         ph, pw = (0, 0)
-    conv_map = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                          'constant', constant_values=0)
-    db = np.sum(dZ, axis=(1, 2, 3), keepdims=True)
-    dW = np.zeros(shape=W.shape)
-    dA = np.zeros(shape=A_prev.shape)    
-    for image in range(m):
-        for h in range(h_new):
-            for w in range(w_new):
-                for f in range(c_new):
-                    fil = W[:, :, :, f]
-                    dz = dZ[image, h, w, f]
-                    a = A_prev[image, h * sh : h * sh + kh, w * sw : w * sw + kw, :]
-                    dW[:, :, :, f] += a * dz
-                    dA[image, h * sh : h * sh + kh, w * sw : w * sw + kw, :] += dz * fil
-    dA = dA[:, ph:-ph, pw:-pw, :]
-    return (dA, dW, db)
+    padded_image = np.pad(A_prev,
+                        pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                        mode="constant")
+    dA_prev = np.zeros(padded_image.shape)
+    dW = np.zeros(W.shape)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+    for img in range(m):
+        for i in range(zh):
+            x = i * sh
+            for j in range(zw):
+                y = j * sw
+                for k in range(zc):
+                    dz = dZ[img, i, j, k]
+                    slice_img = padded_image[img, x: x + kh, y: y + kw, :]
+                    dW[:, :, :, k] += dz * slice_img
+                    dA_prev[img, x: x + kh, y: y + kw, :] += dz * W[:, :, :, k]
+    if padding == "same":
+        dA_prev = dA_prev[:, ph:-ph, pw:-pw, :] 
+    else:
+        pass
+    return (dA_prev, dW, db)
