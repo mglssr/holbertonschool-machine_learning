1e11d69a5f1ecf85386d3c619f720b2a1c966f5e
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-02-02 13:55:48 +0000

    task 0

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -12,19 +12,17 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     if padding == 'same':
         ph = int((h_prev - 1) * sh + kh - h_prev // 2)
         pw = int((w_prev - 1) * sw + kw - w_prev // 2)
-    else:
-        ph, pw = (0, 0)
-    oh = int(((h_prev + 2 * ph - kh) // sh) + 1)
-    ow = int(((w_prev + 2 * pw - kw) // sw) + 1)
+    oh = int((h_prev + 2 * ph - kh) // sh + 1)
+    ow = int((w_prev + 2 * pw - kw) // sw + 1)
     output = np.zeros((m, oh, ow, c_new))
-    padded_image = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                          'constant', constant_values=0)
+    padded_image = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
     for i in range(oh):
-        x = sh * i
         for j in range(ow):
-            y = sw * j
-            image_slice = padded_image[:, x: x + kh, y: y + kw, :]
-            for k in range(c_new):
-                output[:, i, j, k] = np.sum(
-                    image_slice * W[:, :, :, k], axis=(1, 2, 3))
+            for z in range(c_new):
+                x = i * sh
+                y = j * sw
+                w = W[:, :, :, z]
+                output[:, i, j, z] = np.sum((padded_image[:, x:
+                                                          x + kh, y: y + kw, :]
+                                             * w), axis=(1, 2, 3))
     return (activation(output + b))
