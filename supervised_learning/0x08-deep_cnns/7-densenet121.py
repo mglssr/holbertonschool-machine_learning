aacc28faa9e74a8a711016e25cc1fc6ada6bf3dc
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2023-02-10 02:50:32 +0000

    lg

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -8,20 +8,16 @@ transition_layer = __import__('6-transition_layer').transition_layer
 def densenet121(growth_rate=32, compression=1.0):
     """function that builds the DenseNet-121 architecture as
     described in https://arxiv.org/pdf/1608.06993.pdf"""
-    init = K.initializers.he_normal()
-    act = {"kernel_initializer": init,
-           "padding": "same"}
-    X = K.Input(shape=(224, 224, 3))
-    nb_filters = growth_rate * 2
-    o = K.layers.BatchNormalization()(X)
-    o = K.layers.Activation('relu')(o)
-    o = K.layers.Conv2D(64, 2, strides=2, **act)(o)
-    o = K.layers.MaxPooling2D(pool_size=3, strides=2, padding='same')(o)
+    init, L = K.initializers.he_normal(), K.layers
+    act = {"kernel_initializer": init, "padding": "same"}
+    X, nb_filters = K.Input(shape=(224, 224, 3)), growth_rate * 2
+    out = L.Activation('relu')(L.BatchNormalization()(X))
+    out = L.Conv2D(64, 7, strides=2, **act)(out)
+    out = L.MaxPooling2D(pool_size=3, strides=2, padding="same")(out)
     for i in [6, 12, 24]:
-        o, nb_filters = dense_block(o, nb_filters, growth_rate, i)
-        o, nb_filters = transition_layer(o, nb_filters, compression)
-    o, nb_filters = dense_block(o, nb_filters, growth_rate, compression)
-    o = K.layers.AveragePooling2D(7, 1, padding='valid')(o)
-    o = K.layers.Dense(1000, activation='softmax', kernel_initializer=init)(o)
-    model = K.models.Model(inputs=X, outputs=o)
-    return model
+        out, nb_filters = dense_block(out, nb_filters, growth_rate, i)
+        out, nb_filters = transition_layer(out, nb_filters, compression)
+    out, nb_filters = dense_block(out, nb_filters, growth_rate, 16)
+    out = L.AveragePooling2D(7, 1, padding='valid')(out)
+    out = L.Dense(1000, activation='softmax', kernel_initializer=init)(out)
+    return K.models.Model(inputs=X, outputs=out)
