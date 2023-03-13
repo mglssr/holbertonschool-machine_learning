52d03b933a0945a4009798d202b823fcf9acba8f
mglssr <iglesiasmaia6@gmail.com>
2023-03-13 14:02:11 -0300

    task 1

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 1"""
+import numpy as np
+mean_cov = __import__('0-mean_cov').mean_cov
+
+
+def correlation(C):
+    """function that calculates a correlation matrix"""
+    if type(C) is not np.ndarray:
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    dia = np.diag(C)
+    corr = C / dia
+    return corr
