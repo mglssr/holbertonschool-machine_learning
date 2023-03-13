ea8160ec815be0a943d403b006e1de2e0a877da7
mglssr <iglesiasmaia6@gmail.com>
2023-03-13 14:21:45 -0300

    task 1

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -8,8 +8,20 @@ def correlation(C):
     """function that calculates a correlation matrix"""
     if type(C) is not np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
-    if len(C.shape) != 2:
+
+    n, d = C.shape
+
+    if len(C.shape) != 2 or n != d:
         raise ValueError("C must be a 2D square matrix")
 
-    dia = np.corrcoef(C)
-    return dia
+    corr_mat = np.zeros((d, d))
+
+    diag = np.diag(C)
+
+    for i in range(d):
+
+        for j in range(d):
+
+            corr_mat[i][j] = C[i][j] / np.sqrt(diag[i] * diag[j])
+
+    return corr_mat
