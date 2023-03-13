1e6c1116045c4d09a2e5202f855c477a0f034916
mglssr <iglesiasmaia6@gmail.com>
2023-03-13 14:30:20 -0300

    task 1

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -10,10 +10,7 @@ def correlation(C):
         raise TypeError("C must be a numpy.ndarray")
 
 
-    if len(C.shape) != 2:
-        raise ValueError("C must be a 2D square matrix")
-
-    if C.shape[0] != C.shape[1]:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
     corr_mat = np.zeros((d, d))
