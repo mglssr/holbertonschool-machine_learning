9981756d606dfa0f19303c8065c421f6982d360e
mglssr <iglesiasmaia6@gmail.com>
2023-03-13 15:06:41 -0300

    task 2

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -7,12 +7,12 @@ class MultiNormal():
     """MultiNormal Class"""
     def __init__(self, data):
         """class constructor"""
-        if type(data) != np.ndarray:
+        if type(data) is not np.ndarray:
             raise TypeError("data must be a 2D numpy.ndarray")
 
-        n, d = data.shape
+        d, n = data.shape
         if n < 2:
             raise ValueError("data must contain multiple data points")
 
         self.mean = np.mean(data, axis=1, keepdims=True)
-        self.cov = np.matmul(data - self.mean, data.T - self.mean.T) / (n - 1)
+        self.cov = np.dot(data - self.mean, data.T - self.mean.T) / (n - 1)
