4627c7592676d2eba2e90cfef345d79e339febc2
mglssr <iglesiasmaia6@gmail.com>
2023-03-28 16:35:48 -0300

    task 5

---
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    module
+"""
+import numpy as np
+
+
+def pdf(X, m, S):
+    """ initialize """
+    if ((type(X) != type(m) or type(m) != type(S) or type(X) is not np.ndarray
+         or X.ndim != 2 or m.ndim != 1 or S.ndim != 2)):
+        return None
+    d = X.shape[1]
+    if m.shape[0] != d or S.shape[0] != d or S.shape[1] != d:
+        return None
+    try:
+        deter = np.linalg.det(S)
+        if deter == 0:
+            return None
+        xmean = (X - m).T
+        numer = (xmean * np.matmul(np.linalg.inv(S), xmean)).sum(axis=0)
+        numer = np.exp(numer / -2)
+        denom = np.sqrt(np.power(2 * np.pi, d) * deter)
+        result = (numer / denom)
+        result = np.maximum(result, 1e-300)
+        return result
+    except Exception:
+        return None
+    
