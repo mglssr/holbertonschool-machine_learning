231bea636d3c83c6e8c50375ef4a3e1badda7b3a
mglssr <iglesiasmaia6@gmail.com>
2023-07-07 13:18:35 -0300

    pycode and sign checks

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -8,15 +8,16 @@ def likelihood(x, n, P):
     hypothetical probabilities of developing severe side effects"""
     if n <= 0:
         raise ValueError("n must be a positive integer")
-    if x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
-    if x > n:
+    if int(x) < 0:
+        raise ValueError("x must be an integer that is\
+                        greater than or equal to 0")
+    if int(x) > n:
         raise ValueError("x cannot be greater than n")
     if P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     L = P.tolist()
     for i in L:
-        if i < 0 or  i > 1:
+        if i < 0 or i > 1:
             raise ValueError("All values in P must be in the range [0, 1]")
     f = np.math.factorial
     comb = f(n) / (f(n - x) * f(x))
